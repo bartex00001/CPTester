449a2d79b98f9ebf3190d42c64e02735d78cf7a0
Bartłomiej Stefański <30830383+bartex00001@users.noreply.github.com>
2022-08-21 17:22:07 +0200

    Answer checker + output colors

--- tester.py
+++ tester.py
@@ -1,26 +1,48 @@
-from enum import Flag
 import time
-import os
-from pathlib import Path
+from os import system
+from os import remove
+from os import listdir
+from os import path
 
-
-INPUT_FILES_FOLDER = "./tests/in/"
-OUTPUT_FILES_FOLDER = "./tests/out/"
+# Time taken is an aproximation.
+TIME_LIMIT_S = 1
+INPUT_FILES_FOLDER = "tests/in/"
+OUTPUT_FILES_FOLDER = "tests/out/"
 TEST_PREFIX = "sol"
 
 IN_TEST_SUFIX = ".in"
 OUT_TEST_SUFIX = ".out"
 
-EXECUTABLE_PATH = "./sol.exe"
+EXECUTABLE_PATH = "sol.exe"
 # Leave empty if you want to compile the executable manually
 SOURCE_PATH = "sol.cpp"
 COMPILE_COMMAND = "g++ -std=c++17 -O2 -o"
 ALWAYS_COMPILE = False
 
 STOP_ON_FAIL = True
+SHOW_FILE_DIFF = True
+TRIM = 15
 
+
+# Custom checker can be defined here.
+# Test runner takes `or` of answer_checker and program_output==file_output
+def answer_checker(input_file: str, program_output_file: str) -> bool: 
+    return False
+
+
+class Colors:
+    RED = "1;31;40"
+    M_RED = "0;30;41"
+    GREEN = "1;32;40"
+    M_GREEN = "0;30;42"
+
+    [staticmethod]
+    def add_color(text: str, color_code: str) -> str:
+        return "\x1b[" + color_code + "m" + text + "\x1b[0m"
 
 class Test:
+    TEMP_OUT_FILE_NAME = "temp.out"
+
     [staticmethod]
     def is_valid_test(input_file_name: str) -> bool:
         output_file_name = input_file_name.replace(IN_TEST_SUFIX, OUT_TEST_SUFIX)
@@ -41,15 +63,121 @@ class Test:
     [staticmethod]
     def is_valid_output(output_file_name: str) -> bool:
         output_file_path = OUTPUT_FILES_FOLDER + output_file_name
-        return os.path.isfile(output_file_path)
+        return path.isfile(output_file_path)
             
 
     def __init__(self, input_file_name) -> None:
-        pass
+        self.input_file_path = INPUT_FILES_FOLDER + input_file_name
+        self.output_file_path = OUTPUT_FILES_FOLDER + input_file_name.replace(IN_TEST_SUFIX, OUT_TEST_SUFIX)
+        self.test_output_path = OUTPUT_FILES_FOLDER + Test.TEMP_OUT_FILE_NAME
+
+        self.test_name = input_file_name.replace(IN_TEST_SUFIX, "")
+        self.command = EXECUTABLE_PATH + " < " + self.input_file_path + " > " + self.test_output_path
 
 
     def run_test(self) -> bool:
-        pass
+        start_time = time.time()
+        system(self.command)
+        end_time = time.time()
+
+        time_elapsed = max(round(end_time - start_time, 3)-0.05, 0)
+        if time_elapsed > TIME_LIMIT_S:
+            self.tle_message(time_elapsed)
+            return False
+
+        if not self.check_output() and not answer_checker(self.input_file_path, self.test_output_path):
+            self.print_wa()
+            if SHOW_FILE_DIFF:
+                self.print_diff()
+            
+            return False
+
+        print(Colors.add_color("✅ Test " + self.test_name + " passed in " + str(time_elapsed) + " s", Colors.GREEN))
+        return True
+
+    
+    def check_output(self) -> bool:
+        expected = ""
+        actual = ""
+
+        with open(self.output_file_path, "r") as file:
+            expected = file.read().strip()
+        
+        with open(self.test_output_path, "r") as file:
+            actual = file.read().strip()
+
+        return expected == actual
+
+    
+    def print_wa(self) -> None:
+        print(Colors.add_color("❌ ❌ Test " + self.test_name + " failed", Colors.RED))
+        print(Colors.add_color("❌ ❌ The input file was: " + self.input_file_path, Colors.RED))
+        print("Input file contents: ")
+        Test.print_file(self.input_file_path)
+        
+
+    def tle_message(self, time_elapsed: float) -> None:
+        print(Colors.add_color("❌ 🕘 Test " + self.test_name + " timed out in " + str(time_elapsed) + " s", Colors.RED))
+        print(Colors.add_color("❌ 🕘 The input file was: " + self.input_file_path, Colors.RED))
+
+
+    def print_diff(self) -> None:
+        with open(self.output_file_path, "r") as file:
+            expected = file.readlines()
+        
+        with open(self.test_output_path, "r") as file:
+            actual = file.readlines()
+
+        print("\nDifferences in output:")
+        lines_to_show = min(min(len(expected), len(actual)), TRIM)
+        for i in range(lines_to_show):
+            Test.print_line_diff(expected[i], actual[i], i+1)
+
+    
+    def print_line_diff(line_expected: str, line_actual: str, line_nr: int) -> None:    
+        correct_line = str(line_nr) + "\t| "
+        incorrect_line = "\t| "
+
+        line_expected = line_expected.strip()
+        line_actual = line_actual.strip()
+
+        if line_expected == line_actual:
+            print(correct_line + line_expected)
+
+        (line_expected, line_actual) = Test.fill_minning_length(line_expected, line_actual)
+
+        for i in range(len(line_actual)):
+            if line_expected[i] == line_actual[i]:
+                correct_line += Colors.add_color(line_expected[i], Colors.GREEN)
+                incorrect_line += Colors.add_color(line_actual[i], Colors.RED)
+            else:
+                correct_line += Colors.add_color(line_expected[i], Colors.M_GREEN)
+                incorrect_line += Colors.add_color(line_actual[i], Colors.M_RED)
+
+        print(correct_line)
+        print(incorrect_line)
+
+    
+    [staticmethod]
+    def fill_minning_length(line_expected: str, line_actual: str):
+        if len(line_expected) < len(line_actual):
+            line_expected += " " * (len(line_actual) - len(line_expected))
+        elif len(line_expected) > len(line_actual):
+            line_actual += " " * (len(line_expected) - len(line_actual))
+
+        return (line_expected, line_actual)
+
+
+    [staticmethod]
+    def print_file(file_path: str) -> None:
+        with open(file_path, "r") as file:
+            contents = file.readlines()
+            for i in range(min(len(contents), TRIM)):
+                print(str(i+1) + "\t| " + contents[i])
+
+            if len(contents) > TRIM:
+                print("...")
+                print("Rest of the file can be found in " + file_path + "\n")
 
 
 class Tester:
@@ -62,7 +190,7 @@ class Tester:
         if EXECUTABLE_PATH == "":
             raise Exception("No executable path specified")
 
-        should_compile = ALWAYS_COMPILE or not os.path.isfile(EXECUTABLE_PATH)
+        should_compile = ALWAYS_COMPILE or not path.isfile(EXECUTABLE_PATH)
 
         if should_compile:
             self.compile_executable()
@@ -72,29 +200,35 @@ class Tester:
         if SOURCE_PATH == "":
             raise Exception("No source code path specified - it is required to compile the executable.\nIf you do not want to compile the executable, check your settings.")
 
-        if os.path.isfile(EXECUTABLE_PATH):
-            os.remove(EXECUTABLE_PATH)
+        if path.isfile(EXECUTABLE_PATH):
+            remove(EXECUTABLE_PATH)
 
-        os.system(COMPILE_COMMAND + " " + EXECUTABLE_PATH + " " + SOURCE_PATH)
+        system(COMPILE_COMMAND + " " + EXECUTABLE_PATH + " " + SOURCE_PATH)
         print("Source compiled to " + EXECUTABLE_PATH)
 
 
     def add_all_tests(self) -> None:
-        for element in os.listdir(INPUT_FILES_FOLDER):
+        for element in listdir(INPUT_FILES_FOLDER):
             file_with_path = INPUT_FILES_FOLDER + element
-            if os.path.isfile(file_with_path):
+            if path.isfile(file_with_path):
                 self.add_test(element)
+
+        print("Tests found: " + str(len(self.tests)))
 
 
     def add_test(self, input_file_name: str) -> None:
         if Test.is_valid_test(input_file_name):
             self.tests.append(Test(input_file_name))
-            print("Added test: " + input_file_name)
 
 
     def run_all_tests(self) -> None:
-        pass
+        for test in self.tests:
+            result = test.run_test()
+            if not result and STOP_ON_FAIL:
+                break
 
 
 if __name__ == "__main__":
     tester = Tester()
+    tester.add_all_tests()
+    tester.run_all_tests()
